6eedcf6ef26e29d511c1516bbc9fabcc0baff03d
GimisLogos <dimitriosriz1924@gmail.com>
2026-04-26 22:41:55 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.com.example.medicaloffice {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.medicaloffice to javafx.fxml;
-    exports com.example.medicaloffice;
-}
